f47f7d276bb922959221f0654860c427d38b3b07
zhuoqun-chen <zqchen33@gmail.com>
2025-06-27 00:35:57 -0400

    update logic to normal entrypoint if no inputs provided

--- install.sh
+++ install.sh
@@ -17,25 +17,44 @@ function install-bins() {
     done
 }
 
-install-bins wget curl expect git gh
-if ! binary-found "chezmoi"; then
-    sudo sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /usr/local/bin
-fi
+function main() {
 
-echo "$GITHUB_TOKEN" | gh auth login -p ssh --hostname github.com --with-token
+    # shellcheck disable=SC2034
+    local target_shell="/bin/zsh"
 
-dotroot="${HOME}"/.local/share/chezmoi
-gh repo clone zhuoqun-chen/dotfiles "$dotroot"
-mkdir -p "${HOME}"/.config/chezmoi
+    # if two inputs are not set, directly exec "/bin/zsh" or exec "$@"
+    if [[ -z "$GITHUB_TOKEN" || -z "$AGE_PASSPHRASE" ]]; then
+        echo "GITHUB_TOKEN and AGE_PASSPHRASE must be set."
+        if [[ $# -eq 0 ]]; then
+            exec "$target_shell"
+        else
+            exec "$@"
+        fi
+        return 0
+    fi
 
-# this step requires `AGE_PASSPHRASE` to be set
-expect "${dotroot}"/pvc_home/decrypt-key.exp
-sleep 1
-chezmoi init --source="${dotroot}" --apply
-echo ".config/git/config" >> "${dotroot}"/home/.chezmoiignore.tmpl
+    install-bins wget curl expect git gh
+    if ! binary-found "chezmoi"; then
+        sudo sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /usr/local/bin
+    fi
 
-if [[ $# -eq 0 ]]; then
-    exec "/bin/zsh"
-else
-    exec "$@"
-fi
+    echo "$GITHUB_TOKEN" | gh auth login -p ssh --hostname github.com --with-token
+
+    dotroot="${HOME}"/.local/share/chezmoi
+    gh repo clone zhuoqun-chen/dotfiles "$dotroot"
+    mkdir -p "${HOME}"/.config/chezmoi
+
+    # this step requires `AGE_PASSPHRASE` to be set
+    expect "${dotroot}"/pvc_home/decrypt-key.exp
+    sleep 1
+    chezmoi init --source="${dotroot}" --apply
+    echo ".config/git/config" >> "${dotroot}"/home/.chezmoiignore.tmpl
+
+    if [[ $# -eq 0 ]]; then
+        exec "$target_shell"
+    else
+        exec "$@"
+    fi
+}
+
+main "$@"
