78fd4bb454f1124111189d04686de3eb566b75fe
zhuoqun-chen <zqchen33@gmail.com>
2025-06-27 12:03:45 -0400

    refactor: dynamic target shell & remove `gh`

--- install.sh
+++ install.sh
@@ -4,12 +4,14 @@
 # - GITHUB_TOKEN (clone repo)
 # - AGE_PASSPHRASE (decrypt)
 
+GITHUB_USERNAME=${GITHUB_USERNAME:="zhuoqun-chen"}
+dotroot="${HOME}"/.local/share/chezmoi
+
 function binary-found() {
 	command -v "$1" >/dev/null 2>&1
 }
 
 function install-bins() {
-    sudo apt update
     for bin in "$@"; do
         if ! binary-found "$bin"; then
             sudo apt install -y "$bin"
@@ -19,12 +21,21 @@ function install-bins() {
 
 function main() {
 
-    # shellcheck disable=SC2034
-    local target_shell="/bin/zsh"
+    local target_shell
+    if binary-found "/bin/zsh"; then
+        target_shell="/bin/zsh"
+    elif binary-found "/bin/bash"; then
+        target_shell="/bin/bash"
+    elif binary-found "/bin/sh"; then
+        target_shell="/bin/sh"
+    else
+        echo "No supported shell found, exiting."
+        return 1
+    fi
 
     # if two inputs are not set, directly exec "/bin/zsh" or exec "$@"
     if [[ -z "$GITHUB_TOKEN" || -z "$AGE_PASSPHRASE" ]]; then
-        echo "GITHUB_TOKEN and AGE_PASSPHRASE must be set."
+        echo "GITHUB_TOKEN and AGE_PASSPHRASE not set, skipping dotfiles init."
         if [[ $# -eq 0 ]]; then
             exec "$target_shell"
         else
@@ -33,15 +44,19 @@ function main() {
         return 0
     fi
 
-    install-bins wget curl expect git gh
+    sudo apt update
+    install-bins curl expect git
     if ! binary-found "chezmoi"; then
         sudo sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /usr/local/bin
     fi
 
-    echo "$GITHUB_TOKEN" | gh auth login -p ssh --hostname github.com --with-token
+    # deprecated: found the base ubuntu:22-jammy image apt-installed-gh doesn't support `-p` flag
+    # install-bins gh
+    # echo "$GITHUB_TOKEN" | gh auth login -p ssh --hostname github.com --with-token
+    # gh repo clone "$GITHUB_USERNAME"/dotfiles "$dotroot"
 
-    dotroot="${HOME}"/.local/share/chezmoi
-    gh repo clone zhuoqun-chen/dotfiles "$dotroot"
+    git clone https://"${GITHUB_TOKEN}"@github.com/"${GITHUB_USERNAME}"/dotfiles.git "$dotroot"
+
     mkdir -p "${HOME}"/.config/chezmoi
 
     # this step requires `AGE_PASSPHRASE` to be set
