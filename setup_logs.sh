00c10f65abeaa8dd2556606934da0fa7a3e76d9f
zhuoqun-chen <zqchen33@gmail.com>
2025-06-29 14:23:36 -0400

    fix: now use ansi2txt to filter

--- setup_logs.sh
+++ setup_logs.sh
@@ -8,13 +8,23 @@ function main() {
     mkdir -p "$logdir" || (echo "Failed to create log directory: $logdir" && exit 1)
     ts=$(date +"%Y.%m.%d-%H.%M.%S")
     logfile="${logdir}/$ts-debug.log"
-    # exec > >(tee -a "$logfile") 2>&1
 
     # filter out ANSI escape codes from the log to make it human-readable
     # m: color codes
     # G: cursor movement
     # K: line clearing
     # H: cursor positioning
-    exec > >(tee >(sed -r "s/\x1B\[[0-9;]*[mGKH]"// > "$logfile")) 2>&1
+    # exec > >(tee >(sed -r "s/\x1B\[[0-9;]*[mGKH]"// > "$logfile")) 2>&1
+
+    if command -v ansi2txt >/dev/null; then
+        # echo "Using ansi2txt to filter ANSI escape codes."
+        exec > >(tee >(ansi2txt > "$logfile")) 2>&1
+    else
+        # echo "ansi2txt not found, using sed as fallback."
+        # ansi2txt() {
+        #     sed -r "s/\x1B\[[0-9;]*[mGKH]//g"
+        # }
+        exec > >(tee -a "$logfile") 2>&1
+    fi
 }
 main "$@"
