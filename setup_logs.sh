7a37587c9116e344a1f6c35648d51337036ac43d
zhuoqun-chen <zqchen33@gmail.com>
2025-06-29 13:06:17 -0400

    try to filter ANSI in logs

--- setup_logs.sh
+++ setup_logs.sh
@@ -8,6 +8,13 @@ function main() {
     mkdir -p "$logdir" || (echo "Failed to create log directory: $logdir" && exit 1)
     ts=$(date +"%Y.%m.%d-%H.%M.%S")
     logfile="${logdir}/$ts-debug.log"
-    exec > >(tee -a "$logfile") 2>&1
+    # exec > >(tee -a "$logfile") 2>&1
+
+    # filter out ANSI escape codes from the log to make it human-readable
+    # m: color codes
+    # G: cursor movement
+    # K: line clearing
+    # H: cursor positioning
+    exec > >(tee >(sed -r "s/\x1B\[[0-9;]*[mGKH]"// > "$logfile")) 2>&1
 }
 main "$@"
